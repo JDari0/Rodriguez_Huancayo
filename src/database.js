0f6a24930519491982256634f0e87ec985f8921b
JDari0 <89716225+JDari0@users.noreply.github.com>
2023-01-30 16:28:57 -0500

    Autenticacion de usuarios y relacion de Schema de rol al Schema de usuario

--- src/database.js
+++ src/database.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 mongoose.set('strictQuery', true);
 mongoose.connect("mongodb://127.0.0.1/companydb",{
    useNewUrlParser: true,
-   useUnifiedTopology: true
+   useUnifiedTopology: true,
+   //useFindAndModify: true
+   //useCreateIndex: true
 })
    
    .then(db => console.log("DB is connected"))
---
+++ src/models/Role.js
@@ -0,0 +1,13 @@
+//Modelo de roles
+import {Schema, model } from "mongoose";
+
+const roleSchema = new Schema(
+   {
+      name: String,
+   },
+   {
+      versionKey: false,
+   }
+);
+
+export default model("Role", roleSchema)
